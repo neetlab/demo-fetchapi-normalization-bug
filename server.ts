10872e760091b79f1925a6627a130339133f1305
Ryo Igarashi <n33t5hin@gmail.com>
2023-10-02 20:29:30 +0900

    Prettify

--- server.ts
+++ server.ts
@@ -1,41 +1,36 @@
-const createHeaders = (override: HeadersInit = {}) =>
-  new Headers({
-    Vary: "Origin",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Methods": "*",
-    ...override,
-  });
-
 Deno.serve((req, _info) => {
   const responseBody = JSON.stringify({
-    message: "Hello!",
+    message: "OK",
   });
 
   switch (req.method) {
     case "OPTIONS":
-      return new Response(null, {
-        status: 204,
-        headers: createHeaders(),
-      });
     case "GET":
-      return new Response(responseBody, {
-        status: 200,
-        headers: createHeaders(),
-      });
     case "PATCH":
-      return new Response(responseBody, {
-        status: 200,
-        headers: createHeaders(),
-      });
     case "POST":
     case "PUT":
     case "DELETE":
     case "HEAD":
+      return new Response(responseBody, {
+        status: 200,
+        headers: new Headers({
+          Vary: "Origin",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Content-Type": "application/json",
+        }),
+      });
     default:
       return new Response(null, {
         status: 405,
-        headers: createHeaders({ "Content-Type": "text/plain" }),
+        headers: new Headers({
+          Vary: "Origin",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Content-Type": "text/plain",
+        }),
       });
   }
 });
